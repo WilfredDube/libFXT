0aa328ff31fd56d381a45f50c560aa4b16f12087
Wilfred Dube <wilfreddube@gmail.com>
2018-08-12 23:22:59 +0200

    Change return type to reduce processes.

--- lib/iges_parser/include/fxt_utils.h
+++ lib/iges_parser/include/fxt_utils.h
@@ -1,7 +1,9 @@
 #ifndef FXT_UTILS_H
 #define FXT_UTILS_H
 
+#include <stdlib.h>
+
 float utils_to_float(char *str);
-char* utils_replace_char(char* str, char find, char replace);
+float utils_replace_char(char* str, char find, char replace);
 
 #endif
