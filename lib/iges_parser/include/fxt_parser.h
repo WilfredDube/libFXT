583ae8bad47f5c2245b889a4d5aa585295f2f667
Wilfred Dube <wilfreddube@gmail.com>
2018-08-13 22:13:27 +0200

    Added new function prototypes for vertix, edge and loop extraction.

--- lib/iges_parser/include/fxt_parser.h
+++ lib/iges_parser/include/fxt_parser.h
@@ -13,7 +13,10 @@ struct _PsectionEntityData {
 };
 
 char *dsection_get_model_space_curve_type(int form_number);
-Vertex *dsection_get_vertex(int start_vertex_ptr, int index);
+
+Vertex *dsection_get_vertex(int entity_ptr, int index);
+Edge *dsection_get_edge(int entity_ptr, int index);
+Loop *dsection_get_loop(int entity_ptr);
 
 PsectionEntityData *psection_entity_object_new(const int pd_pointer, const int ENTITY_TYPE, const char *ENTITY_NAME, void * entity_object);
 
