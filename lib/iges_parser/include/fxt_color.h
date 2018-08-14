c8f747cc810368a12836349d5ce34855164354b2
Wilfred Dube <wilfreddube@gmail.com>
2018-08-14 14:45:33 +0200

    Removed function definition.

--- lib/iges_parser/include/fxt_color.h
+++ lib/iges_parser/include/fxt_color.h
@@ -12,7 +12,6 @@ struct _Color {
 };
 
 PsectionEntityData *color_extract(const int pd_pointer, char *ps, int colornum);
-static Color *color_new(float r, float g, float b, char *name);
 Color *color_by_number(const unsigned int num);
 char *color_name(const unsigned int num);
 
