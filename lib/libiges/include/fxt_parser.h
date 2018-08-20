96c1955d6b37b3355431bb732f341af2fc0a6b56
Wilfred Dube <wilfreddube@gmail.com>
2018-08-20 07:41:34 +0200

    Added new parser class

--- lib/libiges/include/fxt_parser.h
+++ lib/libiges/include/fxt_parser.h
@@ -3,16 +3,90 @@
 
 #include "fxt_types.h"
 
+#include <stdio.h>
+
+typedef enum unit{
+  INCH = 1,
+  MILLIMTER,
+  FEET,
+  MILES,
+  METER,
+  KILOMETER,
+  MILS,
+  MICRONS,
+  CENTIMETER,
+  MICROINCHES
+} Unit;
+
+typedef enum pattern {
   PNONE,
+  SOLID,
+  DASHED,
+  PHANTOM,
+  CENTERLINE,
+  DOTTED
+} Pattern;
+
 struct _GsectionModelDesc {
+  char param_delimeter;
+  char record_delimeter;
+  char *source_PID;
+  char *filename;
+  char *native_system_PID;
+  char *preprocessor_version;
+  int nbits;
+  int max_float_power;
+  int number_of_sig_digits_float;
+  int max_double_power;
+  int number_of_sig_digits_double;
+  char *receiver_PID;
+  float model_space_scale;
+  Unit unit_flag;
+  char *unit_name;
+  long double max_line_weight;
+  int max_line_weight_gradations;
+  char *date_time_of_generation;
+  long double min_resolution_or_granulation; /**< TODO: Investigate this value. */
+  long double approx_max_coordinate;
+  char *author_name;
+  char *author_organization;
+  int specification_version; /** TODO: enum 0 - 7  */
+  int drafting_standard; /** TODO: enum 0 - 7  */
+  char *created_or_last_modification_time;
+  char *descriptor;
 };
 
 struct _DsectionEntity {
+  int entity_type;
+  int ps_data_ptr;
+  void *entity_object;
+  int structure;
+  Pattern line_font_pattern;
+  int layer_or_level;
+  int view_ptr; /** TODO: Investigate the use of this value. */
+  TransformationMatrix *entity_transmatrix;
+  int label_display_assoc;
+  int status_number;
+  int sequence_number;
+  int line_weight;
+  int color;
+  int ps_line_count;
+  int form_number;
+  char *entity_label;
+  unsigned entity_subscript_number;
 };
 
 struct _PsectionEntityData {
+  int entity_param_ptr;
+  int entity_type;
+  int entity_name;
+  void *entity_data_object;
 };
 
+typedef FILE IgesFile;
+
+void get_gsection(IgesFile *fp, char *line);
+void parser_gsection_new(GsectionModelDesc *gsec_model_desc, char *desc_ptr);
 char *dsection_get_model_space_curve_type(int form_number);
 
 Vertex *dsection_get_vertex(int entity_ptr, int index);
@@ -20,6 +94,5 @@ Edge *dsection_get_edge(int entity_ptr, int index);
 Loop *dsection_get_loop(int entity_ptr);
 
 PsectionEntityData *psection_entity_object_new(const int pd_pointer, const int ENTITY_TYPE, const char *ENTITY_NAME, void * entity_object);
-
 
 #endif
---
+++ lib/libiges/src/fxt_parser.c
@@ -0,0 +1,183 @@
+/**
+ * @file fxt_parser.c
+ * @author  Wilfred Dube <wilfreddube@gmail.com>
+ * @date 16 August 2018
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * https://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ * The Parser class for parsing the iges file.
+ */
+#include "../include/fxt_utils.h"
+#include "../include/fxt_vertex.h"
+#include "../include/fxt_parser.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <glib.h>
+
+#define GSEC_MAX 26
+
+GHashTable *dsection_ht = NULL;
+
+// GHashTable *temp_dsection, *temp_gsection, temp_psection;
+
+int
+read_iges_line(FILE *f, char *s)
+{
+  /* read filerow from iges*/
+  int i;
+  int c;
+  for (i=0; i < 80; i++) {
+    c=getc(f);
+    if (c == EOF) return -1;
+    s[i]=(char) c;
+    if (i==0) { /* eat CR LF in line beginning and reset "i" to zero */
+      switch (s[i]) {
+        case '\x0D': i--; break;
+        case '\x0A': i--; break;
+      }
+    }
+  }
+  return 0;
+}
+
+int
+parser_init(IgesFile *fp, char *filename)
+{
+  char line[81];
+
+  if ((fp = fopen(filename, "rb")) == NULL) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (read_iges_line(fp, line) == -1) {
+    puts("ERROR : End Of File");
+    exit(EXIT_FAILURE);
+  }
+
+  if (line[72] == 'F') {
+    puts("ERROR : Binary Iges file.");
+    exit(EXIT_FAILURE);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+void
+copy_field(char *s1, int poz, int x, char *s2)
+{
+  /* copy "x" chars from position "poz" from string "s1" to string "s2"*/
+  int i, j;
+  i=poz;
+  j=0;
+  // s2 = (char *)malloc(sizeof(s1));
+  while (j < x) {
+    s2[j]=s1[i];
+    i++;
+    j++;
+  }
+}
+
+void
+get_dsection(IgesFile *fp, char *lines)
+{
+  char *line = malloc(700);
+  char *line2 = malloc(700);
+
+  line = fgets(line, 81, fp);
+  while(line != NULL){
+    if(line[72] == 'D'){
+      line = fgets(line, 81, fp);
+      strncat(lines, line, 72);
+      line2 = fgets(line, 81, fp);
+      strncat(lines, line2, 27);
+    } else{
+      line = fgets(line, 81, fp);
+    }
+  }
+}
+
+void
+get_gsection(IgesFile *fp, char *lines)
+{
+  char *line = malloc(700);
+
+  line = fgets(line, 81, fp);
+  while(line != NULL){
+    if(line[72] == 'G'){
+      strncat(lines, line, 72);
+      line = fgets(line, 81, fp);
+    } else{
+      line = fgets(line, 81, fp);
+    }
+  }
+}
+
+void
+parser_gsection_new(GsectionModelDesc *gsec_model_desc, char *desc_ptr)
+{
+  char *desc_array[GSEC_MAX] = {NULL};
+
+  utils_to_array(desc_array, desc_ptr, DELIMITER);
+
+  gsec_model_desc = (GsectionModelDesc *) malloc(sizeof(GsectionModelDesc));
+
+  gsec_model_desc->param_delimeter = ',';
+  gsec_model_desc->record_delimeter = ';';
+  gsec_model_desc->source_PID = (char *)desc_array[2];
+  gsec_model_desc->filename = (char *)desc_array[3];
+  gsec_model_desc->native_system_PID = (char *)desc_array[4];
+  gsec_model_desc->preprocessor_version = (char *)desc_array[5];
+  gsec_model_desc->nbits = utils_to_int(desc_array[6]);
+  gsec_model_desc->max_float_power = utils_to_int(desc_array[7]);
+  gsec_model_desc->number_of_sig_digits_float = utils_to_int(desc_array[8]);
+  gsec_model_desc->max_double_power = utils_to_int(desc_array[9]);
+  gsec_model_desc->number_of_sig_digits_double = utils_to_int(desc_array[10]);
+  gsec_model_desc->receiver_PID = (char *)desc_array[11];
+  gsec_model_desc->model_space_scale = utils_replace_char(desc_array[12], 'D', 'E');
+  gsec_model_desc->unit_flag = (Unit)desc_array[13];
+  gsec_model_desc->unit_name = (char *)desc_array[14];
+  gsec_model_desc->max_line_weight = utils_replace_char(desc_array[15], 'D', 'E');
+  gsec_model_desc->max_line_weight_gradations = utils_to_int(desc_array[16]);
+  gsec_model_desc->date_time_of_generation = (char *)desc_array[17];
+  gsec_model_desc->min_resolution_or_granulation = utils_replace_char(desc_array[18], 'D', 'E'); /**< TODO: Investigate this value. */
+  gsec_model_desc->approx_max_coordinate = utils_replace_char(desc_array[19], 'D', 'E');
+  gsec_model_desc->author_name = (char *)desc_array[20];
+  gsec_model_desc->author_organization = (char  *)desc_array[21];
+  gsec_model_desc->specification_version = utils_to_int(desc_array[22]); /** TODO: enum 0 - 7  */
+  gsec_model_desc->drafting_standard = utils_to_int(desc_array[23]); /** TODO: enum 0 - 7  */
+  gsec_model_desc->created_or_last_modification_time = (char *)desc_array[24];
+  gsec_model_desc->descriptor = (char *)desc_array[25];
+
+  // size_t i = 0;
+  // while (i < 26) {
+  //   printf("%s\n", desc_array[i]);
+  //   i++;
+  // }
+}
+
+void
+parser_dsection_new(DsectionEntity *dsec_entity, char *desc_ptr)
+{
+  char *desc_array[GSEC_MAX] = {NULL};
+
+  utils_to_array(desc_array, desc_ptr, DELIMITER);
+
+  dsec_entity = (DsectionEntity *) malloc(sizeof(DsectionEntity));
+
+}
