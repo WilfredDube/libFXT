b79dc263a40b364357307055d863930f38a1bf21
Wilfred Dube <wilfreddube@gmail.com>
2018-09-03 00:21:15 +0200

    Changed names and function definition.

--- lib/libiges/include/fxt_loop.h
+++ lib/libiges/include/fxt_loop.h
@@ -3,15 +3,16 @@
 
 #include "fxt_types.h"
 
-#define EDGES_MAX 10000
+#define LOOP_MAX 10000
 
 typedef void * List;
 
 struct _Loop {
   int n; /**< Number of loop tuples in list (N > 0)*/
-  List edges[EDGES_MAX]; /**< Array of edges or vertices */
+  List edges[LOOP_MAX]; /**< Array of edges or vertices */
 };
 
-PsectionEntityData *loop_extract(const int pd_pointer, char *ps);
+Loop *loop_extract(char *loop_array[]);
+void print_num(Loop *lp);
 
 #endif
