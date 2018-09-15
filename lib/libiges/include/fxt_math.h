677fb58e4a13372b464b74e20c939eb907f5f96b
Wilfred Dube <wilfreddube@gmail.com>
2018-09-15 19:49:00 +0200

    Added new maths class.

---
+++ lib/libiges/include/fxt_math.h
@@ -0,0 +1,24 @@
+
+#ifndef FXT_MATH_H
+#define  FXT_MATH_H
+
+#include "fxt_types.h"
+#include <stdbool.h>
+
+Vertex *compute_normal(Loop *loop);
+long double compute_length(Edge *edge);
+long double compute_bend_length(Edge *edge);
+float compute_angle(Vertex *norm1, Vertex *norm2);
+bool issideplane(Loop *edge_loop, long double thickness);
+long double compute_thickness(Loop *loop);
+bool is_parallel(Vertex *norm1, Vertex *norm2);
+bool is_distance_thickness(Face *fc1, Face *fc2);
+Loop *central_face_generation(Face *fc);
+long double compute_distance_btw_planes(Face *fc1, Face *fc2);
+long double compute_distance_btw_2_points(Vertex *norm1, Vertex *norm2);
+long double compute_distance_btw_plane_points(Face *fc1, Face *fc2);
+long double compute_gradient(Edge *edge);
+long double compute_bending_force(long double *bendlength, long double thickness, int unit, long double tensile_strength);
+void compute_plane_equation(Face *face);
+
+#endif
---
+++ lib/libiges/src/fxt_math.c
@@ -0,0 +1,43 @@
+/**
+ * @file fxt_math.c
+ * @author  Wilfred Dube <wilfreddube@gmail.com>
+ * @date 16 August 2018
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * https://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ * The Math cpmputation class for all calculations.
+ */
+#include "../include/fxt_math.h"
+#include "../include/fxt_parser.h"
+#include "../include/fxt_vertex.h"
+#include "../include/fxt_edge.h"
+#include "../include/fxt_loop.h"
+#include "../include/fxt_types.h"
+#include <math.h>
+
+/* Function for computing the normal of a plane.
+ * @param loop is the loop representing a plane.
+ * @returns the normal vector
+ */
+Vertex *compute_normal(Loop *loop){
+  // printf("LOOP = %d\n", (int)((Edge *)loop->edges[0])->model_space_curve_type);
+  // Vertex *vt_s = get_start_vertex(((Edge *)loop->edges[0]));
+  // printf("%Lf\n", vt_s->x);
+  // printf("%p\n", loop->edges);
+
+  return NULL;
+}
